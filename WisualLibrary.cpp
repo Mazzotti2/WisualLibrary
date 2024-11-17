e0888ce1931e90b2e991c904853d1c0fbab5dc40
Mazzotti2 <pdawidowski@interia.pl>
2024-11-17 12:39:16 +0100

    Update WisualLibrary.cpp
    Version 0.2.0

--- WisualLibrary.cpp
+++ WisualLibrary.cpp
@@ -1,11 +1,11 @@
 #include "WisualLibrary.hpp"
 
-void WisualLibrary::TextC(short tekst, short background) {
+void WisualLibrary::TextColor(short tekst, short background) {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hConsole, tekst + background * 16);
 }
 
-void WisualLibrary::CursorP(short pos_x, short pos_y) {
+void WisualLibrary::CursorPos(short pos_x, short pos_y) {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleCursorPosition(hConsole, { pos_x, pos_y });
 }
@@ -23,5 +23,29 @@ void WisualLibrary::Rectangle(short length, short width, char wypelnienie) {
 void WisualLibrary::Line(short length, char wypelnienie) {
 	for (int i = 0; i < length; i++) {
 		cout << wypelnienie;
+	}
+}
+
+void WisualLibrary::CursorShow(bool showFlag)
+{
+	HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO     cursorInfo;
+
+	GetConsoleCursorInfo(out, &cursorInfo);
+	cursorInfo.bVisible = showFlag; // set the cursor visibility
+	SetConsoleCursorInfo(out, &cursorInfo);
+}
+
+COORD WisualLibrary::CursorPosGet(HANDLE hConsoleOutput)
+{
+	CONSOLE_SCREEN_BUFFER_INFO cbsi;
+	if (GetConsoleScreenBufferInfo(hConsoleOutput, &cbsi))
+	{
+		return cbsi.dwCursorPosition;
+	}
+	else
+	{
+		COORD invalid = { 0, 0 };
+		return invalid;
 	}
 }
